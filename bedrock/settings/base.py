a7226313ba3ee8618e6c527ec34b075316eefd6d
Alex Gibson <alexgibson@users.noreply.github.com>
2019-05-29 09:55:00 +0100

    Add app.convert.com to CSP_STYLE_SRC

--- bedrock/settings/base.py
+++ bedrock/settings/base.py
@@ -1404,6 +1404,7 @@ CSP_SCRIPT_SRC = CSP_DEFAULT_SRC + [
 CSP_STYLE_SRC = CSP_DEFAULT_SRC + [
     # TODO fix things so that we don't need this
     "'unsafe-inline'",
+    'app.convert.com', # convert A/B
 ]
 CSP_CHILD_SRC = [
     'www.googletagmanager.com',
